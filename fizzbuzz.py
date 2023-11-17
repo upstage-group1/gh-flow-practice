17aa16b517a2ba12ece7a7acca9ffde670303215
RJ <43860568+RachelJun@users.noreply.github.com>
2023-11-17 18:14:11 +0900

    fetaure: refactored original code

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,8 +1,4 @@
-print('fizz')
+for i in range(1, 30 + 1):
+    result = 'fizz' * (i % 3 == 0) + 'buzz' * (i % 5 == 0)
+    print(result if result else i)
 
-for i in range(1, 30+1) :
-    #print('fizz') if i % 3 == 0 else print(i)
-    if i % 3 == 0 or i % 5 == 0 :
-        print('fizz' * (i%3==0) + 'buzz' * (i%5==0))
-    else :
-        print(i)
