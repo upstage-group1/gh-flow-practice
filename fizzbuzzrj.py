ff3685f067e6e2da395cf8d60f01c7df407ea76a
RJ <43860568+RachelJun@users.noreply.github.com>
2023-11-17 17:51:00 +0900

    Revised as requested

--- fizzbuzzrj.py
+++ fizzbuzzrj.py
@@ -1,9 +1,7 @@
 for i in range(1,16+1):
 	if i%15 == 0:
 		print('fizzbuzz')
-	elif i%3 == 0:
-		print('fizz')
-	elif i%5 == 0:
-		print('buzz')
+	elif i%3 == 0 or i%5 == 0:
+		print('fizz' if i% 3 == 0 else 'buzz')
 	else:
 		print(i)
